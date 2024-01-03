4021d70854091aff9ed8e36a294ccb6af9e5c9dc
Kevin Santana <klinarte258@gmail.com>
2024-01-02 22:43:32 -0700

    prueba

--- src/main/java/logica/Horario.java
+++ src/main/java/logica/Horario.java
@@ -6,15 +6,27 @@ public class Horario {
     private int id_horario;
     private Date hora_entrada;
     private Date hora_salidad;
+    
+    //Relacion Odontologo
+    //1 to 1
+    private Odontologo odontologo;
+    
+    //Relacion secretario
+    //1 to 1
+    private Secretario secretario;
 
     public Horario() {
     }
 
-    public Horario(int id_horario, Date hora_entrada, Date hora_salidad) {
+    public Horario(int id_horario, Date hora_entrada, Date hora_salidad, Odontologo odontologo, Secretario secretario) {
         this.id_horario = id_horario;
         this.hora_entrada = hora_entrada;
         this.hora_salidad = hora_salidad;
+        this.odontologo = odontologo;
+        this.secretario = secretario;
     }
+
+
 
     public int getId_horario() {
         return id_horario;
@@ -38,6 +50,22 @@ public class Horario {
 
     public void setHora_salidad(Date hora_salidad) {
         this.hora_salidad = hora_salidad;
+    }
+
+    public Odontologo getOdontologo() {
+        return odontologo;
+    }
+
+    public void setOdontologo(Odontologo odontologo) {
+        this.odontologo = odontologo;
+    }
+
+    public Secretario getSecretario() {
+        return secretario;
+    }
+
+    public void setSecretario(Secretario secretario) {
+        this.secretario = secretario;
     }
     
     
